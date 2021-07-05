7ff2af245c0b2eb37da7c9dbeb5fa2832d8cf7a8
Eddie Saliby <eddysaliby@gmail.com>
2021-07-05 15:17:58 +0200

    fix

--- twitter.py
+++ twitter.py
@@ -3,13 +3,11 @@ import re
 import io
 import csv
 
-
 # Credentials
-consumer_key = "42jQVIDXekhqAJFwx4leynRNg"
-consumer_secret = "VvgjiJqHKKH2RVzQrPLIKbUn3oZWhcluGNl6F2fVI7BF6LIn5c"
-access_token = "864386625752752128-0RWDGHQX1N59XIPNSwqn6fnwelegpAT"
-access_token_secret = "KCh4ulCnCOqbXAjQcZUypNgWPMlUvzOtOFuJQlq9v1meb"
-
+consumer_key = "xxxxxxxxx"
+consumer_secret =  "xxxxxxxxx"
+access_token =  "xxxxxxxxx"
+access_token_secret =  "xxxxxxxxx"
 
 # create OAuthHandler object
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -21,10 +19,16 @@ api = tweepy.API(auth,wait_on_rate_limit=True)
 csvFile = open('file-name', 'a')
 csvWriter = csv.writer(csvFile)
 
-search_words = "edd"      # enter your words
+print('Enter your keyword:')
+search_words = input()      # enter your words
 new_search = search_words + " -filter:retweets"
 
-for tweet in tweepy.Cursor(api.search,q=new_search,count=100,
-                           lang="en",
-                           since_id=0).items():
-    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'),tweet.user.screen_name.encode('utf-8'), tweet.user.location.encode('utf-8')])
+count = 0
+
+for tweet in tweepy.Cursor(api.search,q=new_search,count=20,lang="en", since_id=0).items():
+    count = count + 1
+    print('tweet number', count)
+    if count <= 20:
+        csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'),tweet.user.screen_name.encode('utf-8'), tweet.user.location.encode('utf-8')])
+    else:
+        break;
